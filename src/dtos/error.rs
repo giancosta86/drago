e1f815934a4411eb65be84aedc39758e3487b764
Gianluca Costa <giancosta86@users.noreply.github.com>
2026-08-12 19:44:00 +0200

    Update the Rust toolchain and the dependencies (#4)

--- src/dtos/error.rs
+++ src/dtos/error.rs
@@ -2,8 +2,6 @@ use super::{
     DateError, DecimalError, FractionError, RenminbiError, SignedRangeError, UnsignedRangeError,
 };
 use crate::dto;
-use tsify::JsValueSerdeExt;
-use wasm_bindgen::JsValue;
 
 dto! {
     pub enum RandomParamsError {
@@ -15,12 +13,5 @@ dto! {
         Decimal(DecimalError),
         Renminbi(RenminbiError),
         Date(DateError)
-    }
-}
-
-impl From<RandomParamsError> for JsValue {
-    fn from(error: RandomParamsError) -> Self {
-        JsValue::from_serde(&error)
-            .expect("Serialization to JsValue for errors is expected to work")
     }
 }
